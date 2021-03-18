5e594b3d373813683eee41a8c29895290549b49a
niamhyb <80064411+niamhyb@users.noreply.github.com>
2021-03-18 21:56:07 +0000

    Add files via upload

---
+++ TextMessage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Messaging.Models
+{
+    public class TextMessage
+    {
+        [Required]
+        [StringLength(10, MinimumLength = 10, ErrorMessage = "Number must be 10 digits!")]
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "Number must be 10 digits!")]
+        [DisplayName("To")]
+        public string ToNumber { get; set; }
+        [Required]
+        [StringLength(140, ErrorMessage = "140 characters max!")]
+        [DisplayName("Message")]
+        public string Message { get; set; }
+
+    }
+}
